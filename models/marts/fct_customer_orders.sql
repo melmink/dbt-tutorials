ffc778b9985ef937a50ba049b8195ff0726dc2f7
Melissa Mink <mel.w.mink@gmail.com>
2023-04-28 17:08:34 +0000

    centralized logic and split up models

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,92 +1,60 @@
 with
 
--- Import CTE's
 customers as (
 
-select * from {{ source('jaffle_shop', 'customers') }}
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
-orders as (
-
-select * from {{ source('jaffle_shop', 'orders') }}
-
-),
-
-base_payments as (
-
-select * from {{ source('stripe', 'payment') }}
-
-),
-
--- Logical CTE's
-
-payments as (
-    select 
-        orderid as order_id,
-        max(created) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
-    from base_payments
-    where status <> 'fail'
-    group by 1
-),
-
-paid_orders as (    
-    select
-        orders.id as order_id,
-        orders.user_id as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
-        payments.total_amount_paid,
-        payments.payment_finalized_date,
-        customers.first_name as customer_first_name,
-        customers.last_name as customer_last_name
-    from orders
-    left join payments on orders.id = payments.order_id
-left join customers on orders.user_id = customers.id ),
-
-customer_lifetime_value as (
-    select
-        payments.order_id,
-        sum(paid_orders.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders
-        on payments.customer_id = paid_orders.customer_id 
-        and payments.order_id >= paid_orders.order_id
-    group by 1
-    order by payments.order_id
-),
-
--- Final CTE
-
-final as (
-    select
-        payments.*,
-        row_number() over (order by payments.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by payments.order_id)
-            as customer_sales_seq,
-
-        case 
-            when (
-            rank() over (
-                partition by customer_id
-                order by order_placed_at. order_id
-            ) = 1
-            ) then 'new'
-        else 'return' end as nvsr,
-
-        customer_lifetime_value.clv_bad as customer_lifetime_value,
-        first_value(paid_orders.order_placed_at) over (
-            partition by paid_orders.customer_id
-            order by paid_orders.order_placed_at
-        ) as fdos
-    
-    from paid_orders p
-    left outer join customer_lifetime_value
-        on customer_lifetime_value.order_id = payments.order_id
-    order by order_id
+paid_orders as (
+    select * from {{ ref('int_orders') }}
 )
 
--- Simple Select Statement
+final as (
+
+  select
+    paid_orders.order_id,
+    paid_orders.customer_id,
+    paid_orders.order_placed_at,
+    paid_orders.order_status,
+    paid_orders.total_amount_paid,
+    paid_orders.payment_finalized_date,
+    customers.customer_first_name,
+    customers.customer_last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by paid_orders.order_placed_at,
+        paid_orders.order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by paid_orders.customer_id
+    order by paid_orders.order_placed_at, paid_orders.order_id
+    ) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(paid_orders.total_amount_paid) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(paid_orders.order_placed_at) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as fdos
+
+    from paid_orders
+	left join customers on paid_orders.customer_id = customers.customer_id
+)
 
 select * from final
